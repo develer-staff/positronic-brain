2e9624a348da93bcae0f86b27936b5e67f3f0a63
Lorenzo Villani <lvillani@develer.com>
2014-06-11 19:10:39 +0200

    Add initial test and Travis-CI support

---
+++ positronic/brain/test/__init__.py
@@ -0,0 +1,23 @@
+#
+# Positronic Brain - Opinionated Buildbot Workflow
+# Copyright (C) 2014  Develer S.r.L.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+from positronic.brain.utils import get_default_email_address
+
+
+def test_get_default_email_address():
+    assert get_default_email_address('http://buildbot.example.com') == 'noreply@example.com'
